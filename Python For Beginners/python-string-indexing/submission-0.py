d8005a5467b96c8b1823af93ab38401baeecc55e
jeevan3296 <56495586+jeevan3296@users.noreply.github.com>
2026-05-06 14:59:36 -0700

    Add: python-string-indexing - submission-0

---
+++ Python For Beginners/python-string-indexing/submission-0.py
@@ -0,0 +1,19 @@
+def print_first_char(word: str) -> str:
+    print(word[0])
+
+def print_second_char(word: str) -> str:
+    print(word[1])
+
+def print_last_char(word: str) -> str:
+    l = len(word)
+    print(word[l-1])
+
+
+# do not modify below this line
+print_first_char("hello")
+print_second_char("hello")
+print_last_char("hello")
+
+print_first_char("yay")
+print_second_char("yay")
+print_last_char("yay")
